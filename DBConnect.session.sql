ee55148fde6ffee249881868d0d11961da35e540
JCheney20 <64533742+JCheney20@users.noreply.github.com>
2023-10-21 00:39:48 +0200

    SQL_Update1

---
+++ DBConnect.session.sql
@@ -0,0 +1,26 @@
+--@block
+INSERT INTO users (
+        firstname,
+        surname,
+        email,
+        usertype,
+        gender,
+        cellnumber,
+        dob,
+        username,
+        pass_word
+    )
+VALUES (
+        "Admin",
+        "Admin",
+        "example@test.com",
+        "Admin",
+        "Other",
+        "0735292679",
+        "2002-01-26",
+        "Admin123",
+        "Test1234"
+    );
+--@block
+SELECT *
+FROM users;
---
+++ src/utils_DBManager.java
@@ -0,0 +1,5 @@
+package src;
+
+public class utils_DBManager {
+
+}
